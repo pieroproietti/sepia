c9236a363893074e0419913dfca7b54b7e18aa30
Piero Proietti <piero.proietti@gmail.com>
2023-01-15 16:30:43 +0100

    seppia an OEM installer

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,37 +1,51 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import icon from '../../assets/seppia.png';
 import './App.css';
 
 const Hello = () => {
   return (
     <div>
+      <h1>Cuttle-fish</h1>
+      <h2>OEM installer</h2>
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
       <div className="Hello">
         <a
-          href="https://electron-react-boilerplate.js.org/"
+          href="https://penguins-eggs.net/book/italiano9.3.html"
           target="_blank"
           rel="noreferrer"
         >
           <button type="button">
-            <span role="img" aria-label="books">
-              📚
+            <span role="img" aria-label="Instructions">
+              
             </span>
-            Read our docs
+            Run
           </button>
         </a>
         <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
+          href="https://penguins-eggs.net/book/italiano9.3.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="Instructions">
+
+            </span>
+            Manual
+          </button>
+        </a>
+
+        <a
+          href="https://t.me/penguins_eggs"
           target="_blank"
           rel="noreferrer"
         >
           <button type="button">
             <span role="img" aria-label="folded hands">
-              🙏
+
             </span>
-            Donate
+            Telegram
           </button>
         </a>
       </div>
