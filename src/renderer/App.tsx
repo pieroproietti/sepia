942599c0f1ed20ab926f06838dd145d2cced86a8
Piero Proietti <piero.proietti@gmail.com>
2023-01-15 19:02:03 +0100

    sta prendendo forma... sbadatamente!

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import Title from './components/Title'
 import Buttons from './components/Buttons'
 import Steps from './components/Steps'
 
@@ -8,6 +9,7 @@ const Hello = () => {
   return (
     <div>
       <div className="Hello">
+        <Title />
         <Steps />
         <Buttons />
       </div>
