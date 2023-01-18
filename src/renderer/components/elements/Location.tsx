e2792d2ae60779f74b53420a0bd46e674b837ba8
Piero Proietti <piero.proietti@gmail.com>
2023-01-18 15:41:34 +0100

    region

--- src/renderer/components/elements/Location.tsx
+++ src/renderer/components/elements/Location.tsx
@@ -3,14 +3,60 @@
  * author: Piero Proietti
  * mail: piero.proietti@gmail.com
  */
-import ILocation from '../../interfaces/i-location'
+import * as React from 'react';
 import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+import ILocation from '../../interfaces/i-location'
+
+/**
+ *
+ * @returns
+ */
+function RegionSelect() {
+  const [region, setRegion] = React.useState('');
+  // ['Atlantic', 'Africa', 'America', 'Antarctica', 'Artic', 'Australia', 'Europe', 'India', 'Pacific']
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setRegion(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="region-select-label">Language</InputLabel>
+        <Select
+          labelId="region-select-label"
+          id="region-select"
+          value={region}
+          label="Region"
+          onChange={handleChange}
+        >
+          <MenuItem value={'Atlantic'}>Atlantic</MenuItem>
+          <MenuItem value={'Africa'}>Africa</MenuItem>
+          <MenuItem value={'America'}>America</MenuItem>
+          <MenuItem value={'Antartica'}>Antartica</MenuItem>
+          <MenuItem value={'Artic'}>Artic</MenuItem>
+          <MenuItem value={'Australia'}>Australia</MenuItem>
+          <MenuItem value={'Europe'}>Europe</MenuItem>
+          <MenuItem value={'India'}>India</MenuItem>
+          <MenuItem value={'Pacific'}>Pacific</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
+
 
 export default function Location( { region = 'Europe', zone = 'Rome', language = 'it_IT.UTF-9', dateNumbers = 'it_IT.UTF-9' }: ILocation) {
   return (
     <>
       <Box flexDirection="row">
-        region: {region} <br />
+        <RegionSelect />
         zone: {zone} <br />
         language: {language} <br />
         dateNumbers: {dateNumbers} <br />
