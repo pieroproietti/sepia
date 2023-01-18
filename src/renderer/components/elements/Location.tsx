bef76567b0b599c0d8098af7db11eeb0418c52f9
Piero Proietti <piero.proietti@gmail.com>
2023-01-18 21:04:15 +0100

    a bit bored..

--- src/renderer/components/elements/Location.tsx
+++ src/renderer/components/elements/Location.tsx
@@ -12,45 +12,7 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 import ILocation from '../../interfaces/i-location'
 import LanguageSelect from '../../lib/language-select'
-
-/**
- *
- * @returns
- */
-function RegionSelect() {
-  const [region, setRegion] = React.useState('');
-  // ['Atlantic', 'Africa', 'America', 'Antarctica', 'Artic', 'Australia', 'Europe', 'India', 'Pacific']
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setRegion(event.target.value as string);
-  };
-
-  return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="region-select-label">Region</InputLabel>
-        <Select
-          labelId="region-select-label"
-          id="region-select"
-          value={region}
-          label="Region"
-          onChange={handleChange}
-        >
-          <MenuItem value={'Atlantic'}>Atlantic</MenuItem>
-          <MenuItem value={'Africa'}>Africa</MenuItem>
-          <MenuItem value={'America'}>America</MenuItem>
-          <MenuItem value={'Antartica'}>Antartica</MenuItem>
-          <MenuItem value={'Artic'}>Artic</MenuItem>
-          <MenuItem value={'Australia'}>Australia</MenuItem>
-          <MenuItem value={'Europe'}>Europe</MenuItem>
-          <MenuItem value={'India'}>India</MenuItem>
-          <MenuItem value={'Pacific'}>Pacific</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-  );
-}
-
+import RegionSelect from '../../lib/language-select'
 
 
 export default function Location( { region = 'Europe', zone = 'Rome', language = 'it_IT.UTF-9', dateNumbers = 'it_IT.UTF-9' }: ILocation) {
---
+++ src/renderer/lib/regioni-select.tsx
@@ -0,0 +1,5 @@
+/**
+ * seppia: a first access system configurator
+ * author: Piero Proietti
+ * mail: piero.proietti@gmail.com
+ */
